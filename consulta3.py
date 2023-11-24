6983cabc9134954cc030eeb93f80a281862fea5a
demiandimi <90013442+demiandimi@users.noreply.github.com>
2023-11-24 20:19:56 -0300

    Update consulta3.py

--- consulta3.py
+++ consulta3.py
@@ -9,15 +9,12 @@ PREFIX : <http://ex.org/>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-SELECT ?canaln ?videon (MAX(xsd:integer(?visitas)) AS ?visitat) WHERE {
+SELECT ?canaln (MAX(xsd:integer(?visitas)) AS ?visitasdos) WHERE {
 ?canales <http://ex.org/a#Youtuber> ?canaln .
 ?videos <http://ex.org/a#channelTitle> ?canaln .
 ?videos <http://ex.org/a#view_count> ?visitas .
-?videos <http://ex.org/a#title> ?videon .
 }
-GROUP BY ?canaln
-ORDER BY DESC(?visitat)
+GROUP BY ?canaln 
 """
-#g.query(query)
 for r in g.query(query):
-    print( r["canaln"] + " " + r["videon"] + " " + r["visitat"])
+    print( r["canaln"] +  " " + r["visitasdos"])
